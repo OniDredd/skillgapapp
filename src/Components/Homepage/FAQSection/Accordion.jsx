26a109f3c1a4e3fcd82c91711cc1d25c6d8e83f8
Onidredd <joshuaf@missionreadyhq.com>
2024-01-29 12:52:42 +1300

    Changed a couple files back to jsx.

--- src/Components/Homepage/FAQSection/Accordion.jsx
+++ src/Components/Homepage/FAQSection/Accordion.jsx
@@ -4,14 +4,7 @@ import React from 'react';
 import { Collapse } from "react-collapse";
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-interface AccordionProps {
-    open: boolean;
-    toggle: () => void;
-    title: string;
-    desc: string;
-  }
-
-  const Accordion: React.FC<AccordionProps> = ({ open, toggle, title, desc }) => {
+const Accordion = ({ open, toggle, title, desc }) => {
   return (
     <div className="text-black">
         <div className="bg-white py-5 px-10 flex justify-between items-center cursor pointer text-xl font-semibold border-[1px] border-black rounded-md"
